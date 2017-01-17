9da0fea0694f3e04f4f4508ab9e76474d40d82eb
Bogdan Vasilescu <vasilescu@gmail.com>
2017-01-17 02:03:53 -0500

    missing import

--- homework/1/src/main/java/edu/cmu/cs/cs214/hw1/FindClosestMatch.java
+++ homework/1/src/main/java/edu/cmu/cs/cs214/hw1/FindClosestMatch.java
@@ -1,5 +1,7 @@
 package edu.cmu.cs.cs214.hw1;
 
+import java.io.IOException;
+
 /**
  * Takes a list of URLs on the command line and prints the two URL whose web pages have the highest cosine similarity.
  * Prints a stack trace if any of the URLs are invalid, or if an exception occurs while reading data from
